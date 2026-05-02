ccd9afbf8e0c4819c4f581e2eaaa11ae2e63c0e9
deathCodinn <ilgiz.ilishev@gmail.com>
2026-05-02 21:22:21 +0500

    Modified refresh method in UserController

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -54,9 +54,9 @@ class UserController {
   async refresh(req, res, next) {
     try {
       const { refreshToken } = req.cookies;
-      const token = await userService.refresh(refreshToken);
-      res.clearCookie('refreshToken');
-      return res.json(token);
+      const userData = await userService.refresh(refreshToken);
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+      return res.json(userData);
     } catch (e) {
       next(e);
     }
