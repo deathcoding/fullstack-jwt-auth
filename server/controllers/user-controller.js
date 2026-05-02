4377c2a7ab32a72a2d29b1d1f02df6b6ca042f82
deathCodinn <ilgiz.ilishev@gmail.com>
2026-05-02 17:51:51 +0500

    Add express-validator for input validation and enhance user authentication flow

--- server/controllers/user-controller.js
+++ server/controllers/user-controller.js
@@ -1,12 +1,18 @@
 import userService from "../service/user-service.js";
+import { validationResult } from 'express-validator';
+import ApiError from "../exceptions/api-error.js";
 
 class UserController {
   async registration(req, res, next) {
     try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return next(ApiError.BadRequest('Ошибка при валидациии', errors.array()));
+      }
       const { email, password } = req.body;
       const userData = await userService.registration(email, password);
-      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
 
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
       return res.json(userData);
     } catch (e) {
       next(e);
@@ -15,7 +21,10 @@ class UserController {
 
   async login(req, res, next) {
     try {
-
+      const { email, password } = req.body;
+      const userData = await userService.login(email, password);
+      res.cookie('refreshToken', userData.refreshToken, {maxAge: 30 * 24 * 60 * 60 * 1000, httpOnly: true});
+      return res.json(userData);  
     } catch (e) {
       next(e);
     }
@@ -23,7 +32,10 @@ class UserController {
 
   async logout(req, res, next) {
     try {
-
+      const { refreshToken } = req.cookies;
+      const token = await userService.logout(refreshToken);
+      res.clearCookie('refreshToken');
+      return res.json(token);
     } catch (e) {
       next(e);
     }
@@ -41,7 +53,10 @@ class UserController {
 
   async refresh(req, res, next) {
     try {
-
+      const { refreshToken } = req.cookies;
+      const token = await userService.refresh(refreshToken);
+      res.clearCookie('refreshToken');
+      return res.json(token);
     } catch (e) {
       next(e);
     }
--- server/service/token-service.js
+++ server/service/token-service.js
@@ -20,6 +20,34 @@ class TokenService {
     const token = await tokenModel.create({user: userId, refreshToken});
     return token;
   }
+
+  async removeToken(refreshToken) {
+    const tokenData = await tokenModel.deleteOne({refreshToken});
+    return tokenData;
+  }
+
+  async findToken(refreshToken) {
+    const tokenData = await tokenModel.findOne({refreshToken});
+    return tokenData;
+  }
+
+  validateAccessToken(token) {
+    try {
+      const userData = jwt.verify(token, procces.env.JWT_ACCESS_SECRET);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
+
+  validateRefreshToken(token) {
+    try {
+      const userData = jwt.verify(token, procces.env.JWT_REFRESH_SECRET);
+      return userData;
+    } catch (e) {
+      return null;
+    }
+  }
 }
 
 export default new TokenService();
